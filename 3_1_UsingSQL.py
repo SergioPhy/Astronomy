0f1239a53e48f2815c2afd39abe099e6211a963f
SergioPhy <51884344+SergioPhy@users.noreply.github.com>
2020-08-25 14:36:13 -0500

    Update 3_1_UsingSQL.py

--- 3_1_UsingSQL.py
+++ 3_1_UsingSQL.py
@@ -43,3 +43,19 @@ SELECT MIN(radius), MAX(radius), AVG(radius), STDDEV(radius)
 FROM Planet
 WHERE
   kepler_name IS NULL;
+
+#---------------  
+#You need to find out how many planets in the Planet database are in a multi-planet system. Planets sharing the same star 
+#will have the same kepler_id, but different koi_name values.
+
+#Your query should return a table in which each row contains the kepler_id of the star and the number of planets orbiting 
+#that star (i.e. that share this kepler_id).
+
+#Limit your results to counts above one and order the rows in descending order based on the number of planets.
+#OBS: Here, we are not taking account if the planets have the status of confirmed or not.
+
+SELECT kepler_id, COUNT(koi_name)
+From Planet
+GROUP BY kepler_id
+HAVING COUNT(koi_name) > 1
+ORDER BY COUNT(koi_name) DESC
