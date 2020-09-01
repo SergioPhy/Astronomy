b31ee956c73c1b09588c2ad270c96b676cb95c18
SergioPhy <51884344+SergioPhy@users.noreply.github.com>
2020-08-31 21:24:43 -0500

    Create 3_2_UsingSQL.py

---
+++ 3_2_UsingSQL.py
@@ -0,0 +1,7 @@
+#Abreviations and aliases for the table heads
+
+SELECT s.radius AS sun_radius, p.radius AS planet_radius
+FROM Star AS s, Planet AS p
+WHERE s.kepler_id = p.kepler_id AND
+      s.radius / p.radius > 1
+ORDER BY s.radius DESC;
